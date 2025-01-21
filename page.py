1ffbb63cb2b7d2cca1599e5301fb8f9407f27721
PAVETHRAN BATMANATHEN <102014921+mkpave28@users.noreply.github.com>
2025-01-21 15:51:41 +0800

    Update page.py

--- page.py
+++ page.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from io import StringIO
+from kmodes.kprototypes import KPrototypes  # Add this for K-Prototypes
+import numpy as np 
 
 
 # Set page title and layout
